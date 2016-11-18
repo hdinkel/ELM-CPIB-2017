9b2ee3ad1f06cad7acab267d73018b8db9fe4be3
gouw <marc.gouw@embl.de>
2016-11-18 13:34:52 +0100

    Added merge script and folder to store output.

---
+++ merge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ALL_MD='merged/complete.md'
+
+rm -f $ALL_MD
+
+for i in [1-9]*.md; do
+
+    echo "Adding $i to file."
+    cat $i >> $ALL_MD
+    echo "" >> $ALL_MD
+done
+
+echo ""
+echo "Fixing figure paths"
+sed -i 's#\[\](Figures#\[\](../Figures#' $ALL_MD
+
+echo ""
+echo "Merged output to: $ALL_MD"
