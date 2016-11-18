e13f31eef2203c6d47ac062f29b29e9876724295
gouw <marc.gouw@embl.de>
2016-11-18 15:46:59 +0100

    Modified merge script to make figures (kind of) fit.

--- merge.sh
+++ merge.sh
@@ -20,5 +20,9 @@ echo "Merged output to: $ALL_MD"
 
 cd merged
 pandoc -o complete.tex complete.md
+sed -i 's/includegraphics/includegraphics[width=\\textwidth]/g' complete.tex 
 pdflatex RB-CurrentProtocol_clean.tex
+rm RB-CurrentProtocol_clean.aux
+rm RB-CurrentProtocol_clean.out
+rm RB-CurrentProtocol_clean.log
 cd -
