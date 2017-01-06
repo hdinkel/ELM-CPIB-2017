fc4b374fa64544121ff192c1cbef00fb43f277aa
Marc Gouw <marc.r.gouw@gmail.com>
2017-01-06 15:22:11 +0100

    merge script now runs pdflatex (only)

--- merge.sh
+++ merge.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-ALL_MD='merged/complete.md'
-
-rm -f $ALL_MD
-
-for i in [0-9]*.md; do
-
-    echo "Adding $i to file."
-    cat $i >> $ALL_MD
-    echo "" >> $ALL_MD
-done
-
-echo ""
-echo "Fixing figure paths"
-sed -i.bak 's#(Figures#(../Figures#' $ALL_MD
-sed -i.bak 's/PMID:\([0-9]\{7,8\}\)/\\cite{\1}/g' $ALL_MD
-
-echo ""
-echo "Merged output to: $ALL_MD"
-
-cd merged
-
-pandoc -o complete.tex complete.md
-
-sed -i.bak 's/includegraphics/includegraphics[width=\\textwidth]/g' complete.tex 
-
 pdflatex RB-CurrentProtocol_clean.tex
 bibtex RB-CurrentProtocol_clean
 pdflatex RB-CurrentProtocol_clean.tex
@@ -32,4 +7,3 @@ pdflatex RB-CurrentProtocol_clean.tex
 rm RB-CurrentProtocol_clean.aux
 rm RB-CurrentProtocol_clean.out
 rm RB-CurrentProtocol_clean.log
-cd -
